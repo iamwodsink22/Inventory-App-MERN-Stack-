63b4e90a5be213c784950c9cf9d83904c55e0cd1
iamwodsink22 <109127949+iamwodsink22@users.noreply.github.com>
2024-11-05 18:36:47 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+app.use(cors({ origin: ["https://inventory-app-mern-stack.vercel.app"], credentials: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/user", userRoutes);
